a75d19f8bf99c45ce2dc3b8458f141d10f5e6d9b
Carsten Ditzel <carsten.ditzel@mercedes-benz.com>
2024-09-27 16:34:48 +0200

    add .Xresources

--- install_mgcd.sh
+++ install_mgcd.sh
@@ -24,14 +24,14 @@ make -j$(nproc)
 grep -i $REMOTE_URL .netrc || echo -e "machine $REMOTE_URL \n\t login carsten.ditzel@mercedes-benz.com \n\t password " > ~/.netrc
 echo "git clone "https://carsten.ditzel%40mercedes-benz.com@git-av.nvidia.com/r/a/ndas" && (cd "ndas" && mkdir -p .git/hooks && curl -Lo `git rev-parse --git-dir`/hooks/commit-msg https://carsten.ditzel%40mercedes-benz.com@git-av.nvidia.com/r/tools/hooks/commit-msg; chmod +x `git rev-parse --git-dir`/hooks/commit-msg)"
 
-#ln -svf $CURRENT_DIR/tmux_restore_session.sh ~/tmux_restore_session.sh
 ln -svf $CURRENT_DIR/.bashrc ~/.bashrc
-#ln -svf $CURRENT_DIR/.tmux.conf ~/.tmux.conf
+ln -svf $CURRENT_DIR/.Xresources ~/.Xresources
 ln -svf $CURRENT_DIR/.gitconfig ~/.gitconfig
 ln -svf $CURRENT_DIR/restore_mg.sh ~/.restore_mg.sh
 ln -svf $CURRENT_DIR/.dazelrc ~/.dazelrc
 ln -svf $CURRENT_DIR/user.bazelrc ~/git-ndas/ndas/user.bazelrc
 ln -svf $CURRENT_DIR/.netrc ~/.netrc
+
 
 mkdir -p ${HOME}/.local/bin
 ln -svf $CURRENT_DIR/src/mg ~/.local/bin/mg
